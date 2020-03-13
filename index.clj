ab49b6ba32bafffdd6976e7681d2836cc02f6dce
Henrik Eneroth <henrik@eneroth.com>
2020-03-13 11:53:12 +0100

    Updated site structure

--- index.clj
+++ index.clj
@@ -5,14 +5,10 @@
    [:p description]])
 
 
-[:html
- [:head]
- [:body
-  [:h1 "Fluent."]
-  [:h2 "Welcome to purpose."]
-
-  ;; Top block
-  [:div.content
+;; Site blocks
+;; ##################################
+(def manifesto
+  [:div.block.columns-2
    [:div.paragraph
     [:span.title "Manifesto."]
     "We believe that work is part of life — life should not be lived in breaks between work."]
@@ -21,32 +17,36 @@
    [:div.paragraph "We believe that humans are not resources, and that with the aid of great processes and tools a person can do their best work from wherever they feel they can do their best work."]
    [:div.footnote "[2] We believe that humans are not resources, and that with the aid of great processes and tools a person can do their best work from wherever they feel they can do their best work."]
    [:div.paragraph "Fluent aims to build the platform to facilitate this future workplace. It is a distributed, flexible, virtual workplace, tailor made for co-creation2, which helps connect the best people with the most meaningful projects irregardless of physical location, and takes care of the incidental complexity and overhead of running great projects with great outcomes."]
-   [:div.footnote "[3] We believe that humans are not resources, and that with the aid of great processes and tools a person can do their best work from wherever they feel they can do their best work."]]
+   [:div.footnote "[3] We believe that humans are not resources, and that with the aid of great processes and tools a person can do their best work from wherever they feel they can do their best work."]])
 
-  ;; Middle block
-  [:div.content
+
+(def vision-process
+  [:div.block.columns-2
    [:div.paragraph
     [:span.title "Vision & Process."]
-    "These are a few key parts of the Fluent Process and the Fluent vision."]
-   [:ul
-    [:li "Autonomy, Mastery, and Purpose (AMP3) over hierarchies, forced promotions, and working without caring."]
-    [:li "Fixed appetite and variable scope over fixed scope and false delivery dates."]
-    [:li "Clear, honest, sustainable commitments over \"move fast and break [both people and] things\"."]
-    [:li "Minimizing and automating away overhead to focus on the essential complexities of a project over multitasking between workstuff and busystuff ad insaneum."]
-    [:li "Meaningful projects over maximizing profits."]
-    [:li "Long term thinking over short term panic."]]]
+    "These are a few key parts of the Fluent Process and the Fluent vision."
+    [:ul
+     [:li "Autonomy, Mastery, and Purpose (AMP3) over hierarchies, forced promotions, and working without caring."]
+     [:li "Fixed appetite and variable scope over fixed scope and false delivery dates."]
+     [:li "Clear, honest, sustainable commitments over \"move fast and break [both people and] things\"."]
+     [:li "Minimizing and automating away overhead to focus on the essential complexities of a project over multitasking between workstuff and busystuff ad insaneum."]
+     [:li "Meaningful projects over maximizing profits."]
+     [:li "Long term thinking over short term panic."]]]])
 
-  ;; Bottom block
-  [:div.content
+
+(def inspiration
+  [:div.block.columns-2
    [:div.paragraph
     [:span.title "Inspiration. "]
-    "Here are some things that inspire us:"]
-   [:ul
-    [:li "The Long Now Foundation & the Norrsken Foundation."]
-    [:li "Shape Up & It doesn’t have to be crazy at work, by Basecamp."]
-    [:li "The Byzantine Empire"]]]
+    "Here are some things that inspire us:"
+    [:ul
+     [:li "The Long Now Foundation & the Norrsken Foundation."]
+     [:li "Shape Up & It doesn’t have to be crazy at work, by Basecamp."]
+     [:li "The Byzantine Empire"]]]])
 
-  [:div.components
+
+(def components
+  [:div.block
    (make-component
     {:img-src ""
      :title "AMP"
@@ -62,4 +62,17 @@
    (make-component
     {:img-src ""
      :title "Process"
-     :description "Bla bla bla bla asjkfsd ksdf kasdjf ksadjf lorem ipsum hej hej yo man."})]]]
+     :description "Bla bla bla bla asjkfsd ksdf kasdjf ksadjf lorem ipsum hej hej yo man."})])
+
+
+;; Site structure
+;; ##################################
+[:html
+ [:head]
+ [:body
+  [:h1 "Fluent."]
+  [:h2 "Welcome to purpose."]
+  manifesto
+  vision-process
+  inspiration
+  components]]
